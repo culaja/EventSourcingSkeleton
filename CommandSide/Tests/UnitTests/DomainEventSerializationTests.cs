1b2d403cfe71ea131479853aba0cd17a42712015
Stanko Culaja <stanko.culaja@kolotree.com>
2020-09-05 14:54:57 +0200

    Added common unit tests.

---
+++ CommandSide/Tests/UnitTests/DomainEventSerializationTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Reflection;
+using FluentAssertions;
+using Framework;
+using Xunit;
+using static Newtonsoft.Json.JsonConvert;
+
+namespace Tests.UnitTests
+{
+    public sealed class DomainEventSerializationTests
+    {
+        [Fact]
+        public void all_domain_events_are_correctly_serialized_and_deserialized()
+        {
+            var allDomainEvents = typeof(Values)
+                .GetFields(PublicAndStatic)
+                .Where(FieldIsDomainEvent)
+                .Select(FieldValue)
+                .ToList();
+
+            foreach (var domainEvent in allDomainEvents)
+            {
+                DeserializeObject(SerializeObject(domainEvent), domainEvent.GetType())
+                    .Should().Be(domainEvent);
+            }
+        }
+
+        private static BindingFlags PublicAndStatic => BindingFlags.Static | BindingFlags.Public;
+
+        private static bool FieldIsDomainEvent(FieldInfo fieldInfo) =>
+            typeof(IDomainEvent).IsAssignableFrom(fieldInfo.FieldType);
+
+        private static IDomainEvent FieldValue(FieldInfo fieldInfo) => (IDomainEvent)fieldInfo.GetValue(default);
+    }
+}
---
+++ CommandSide/Tests/UnitTests/ErrorsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Domain;
+using FluentAssertions;
+using Framework;
+using Xunit;
+
+namespace Tests.UnitTests
+{
+    public sealed class ErrorsTests
+    {
+        [Fact]
+        public void Error_codes_must_be_unique()
+        {
+            List<MethodInfo> methods = typeof(Errors).Assembly.GetTypes()
+                .SelectMany(t => t
+                    .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                    .Where(x => x.ReturnType == typeof(Error)))
+                .ToList();
+
+            int numberOfUniqueCodes = methods.Select(GetErrorCode)
+                .Distinct()
+                .Count();
+
+            numberOfUniqueCodes.Should().Be(methods.Count);
+        }
+
+        private string GetErrorCode(MethodInfo method)
+        {
+            object[] parameters = method.GetParameters()
+                .Select<ParameterInfo, object>(x =>
+                {
+                    if (x.ParameterType == typeof(string))
+                        return string.Empty;
+                    
+                    if (x.ParameterType == typeof(long))
+                        return 0;
+
+                    throw new Exception();
+                })
+                .ToArray();
+
+            var error = (Error)method.Invoke(null, parameters);
+            return error.Code;
+        }
+    }
+}
